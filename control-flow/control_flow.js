abbb4e5cb531b734bf4f69be074528dd5bfc4533
giannis zisakis <giannis.zisakis@gmail.com>
2025-07-28 16:10:23 -0400

    feat: Practice Task

--- control-flow/control_flow.js
+++ control-flow/control_flow.js
@@ -45,3 +45,35 @@ console.log("User Category:", userCategory);
 
 let authenticated = isAuthenticated ? 'Authenticated' : 'Not authenticated';
 console.log("Authentication Status:", authenticated);
+
+/* Suppose an organization arranges a "Dietary Services" program to provide diets to its employees and customers, based on a person's weight and day-to-day routine. You need to create an authorization-based code to provide access to people based on their roles in organization, such as employees, enrolled members for "Dietary Services," and subscribers. */
+
+let person = 'Employee'
+let typeOfService;
+let typeOfAccess;
+
+switch(person){
+    case 'Employee':
+        typeOfService = 'Dietary Services';
+        typeOfAccess = 'Full Access';
+        break;
+    case 'Enrolled Member':
+        typeOfService = 'Dietary Services'
+        typeOfAccess = 'One on one interaction with dietician';
+        break;
+    case 'Subscriber':
+        typeOfService = 'Dietary Services'
+        typeOfAccess = 'Partial Access';
+        break;
+        case 'Non-Subscriber':
+            typeOfService = 'No access'
+            typeOfAccess = 'Need to enroll';
+            break;
+        default:
+            typeOfService = 'No access'
+            typeOfAccess = 'No access';
+            break;
+        
+}
+
+console.log(`You have ${typeOfService} and ${typeOfAccess}`)
